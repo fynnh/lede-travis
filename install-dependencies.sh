47e4f8955614c9f89764f75fe72ebbfe01526113
Fynn <fynn.hauptmeier@gmail.com>
2017-11-30 17:02:10 +0100

    Change path

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,3 @@
 sudo apt-get install subversion g++ zlib1g-dev build-essential git python rsync man-db
 sudo apt-get install libncurses5-dev gawk gettext unzip file libssl-dev wget
-export PYTHON=python2 
 git clone https://git.lede-project.org/source.git lede
