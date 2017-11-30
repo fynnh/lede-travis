fd51f831c5d482f9b84ce71709221c2e8322cd0f
Fynn <fynn.hauptmeier@gmail.com>
2017-11-30 15:51:52 +0100

    Add python2

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,4 @@
-sudo apt-get install subversion g++ zlib1g-dev build-essential git python rsync man-db
+sudo apt-get install subversion g++ zlib1g-dev build-essential git python python2 rsync man-db
 sudo apt-get install libncurses5-dev gawk gettext unzip file libssl-dev wget
-export PYTHON=/usr/bin/python 
+#export PYTHON=/usr/bin/python 
 git clone https://git.lede-project.org/source.git lede
