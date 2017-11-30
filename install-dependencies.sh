7bc97867d61c474080601537711398a776096095
Fynn <fynn.hauptmeier@gmail.com>
2017-11-30 15:22:46 +0100

    CAhnge python

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,4 @@
 sudo apt-get install subversion g++ zlib1g-dev build-essential git python rsync man-db
 sudo apt-get install libncurses5-dev gawk gettext unzip file libssl-dev wget
-export PYTHON=/usr/lib/python2.7
+export PYTHON=/usr/local/bin/python
 git clone https://git.lede-project.org/source.git lede
