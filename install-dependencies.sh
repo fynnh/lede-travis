73d32bedc4d7aab6ab10856c4a5b14b9b8ef4967
Fynn <fynn.hauptmeier@gmail.com>
2017-11-30 12:41:43 +0100

    Add tools

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,4 @@
 sudo apt-get install subversion g++ zlib1g-dev build-essential git python rsync man-db
 sudo apt-get install libncurses5-dev gawk gettext unzip file libssl-dev wget
 git clone https://git.lede-project.org/source.git lede
+- make -j3 tools/install V=s
